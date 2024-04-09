7f228d6e6ca018c7a95fef7ce27f6225643bd3f9
Salvatore <87752430+Sal93Git@users.noreply.github.com>
2024-04-10 00:34:25 +1000

    Added Duplication state to agent
    agent blob now performs mitosis

--- Physics_Project_2D/Assets/AI/AiStateMachineAgent.cs
+++ Physics_Project_2D/Assets/AI/AiStateMachineAgent.cs
@@ -8,6 +8,7 @@ public enum AgentState
     Idle,
     Chase,
     Evolve,
+    Mitosis,
     Die,
 }
 
@@ -18,22 +19,82 @@ public class AiStateMachineAgent : MonoBehaviour
     public AgentState currentState;
     public float speed = 5;
     public TileNode target;
-    public int maximumBlobSizeIndex = 0;
+    public int maximumBlobSizeIndex = 5;
+    public GameObject agent;
 
-    private int currentBlobSizeIndex = 0;
+    private int previousDestinationReachedCount = 0;
+    private int destinationReachedCount = 0;
+    public int currentBlobSizeIndex = 0;
     private List<TileNode> path = new List<TileNode>();
     private Vector2 destination;
     private Coroutine gameCoroutine = null;
     private GridMapGenerator gridGen;
+    private bool agentIsActive = false;
+    private bool agentBusy = false;
     //public TileNode[,] grid;
     
     // Start is called before the first frame update
     void Start()
     {
-        currentState = AgentState.Idle;
         astar1 = gameObject.GetComponent<Astar1>();
         gridGen = GameObject.FindObjectOfType<GridMapGenerator>();
+        if(!agentIsActive)
+        {
+            agentIdleStartUp();
+        }
+        else
+        {
+            activateAgentBehaviour();
+        }
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        //A way for user input to begin agent behaviour
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!agentIsActive)
+            {
+                gameCoroutine =  StartCoroutine(TravelState(target));
+                agentIsActive = true;
+            }
+        }
+       
+        // do checks for state change condition
+        if(lifeExpectancyReached())
+        {
+            Debug.Log("lifeExpectancyReached is true");
+            updateStateCoroutine(DieState());
+        }
+        else if(readyToTravel())
+        {
+            Debug.Log("readyToTravel is true");
+            updateStateCoroutine(TravelState(target=getNewRandomDestination()));
+        }
+        else if(atDestination())
+        {
+            Debug.Log("atDestination is true");
+            if(currentBlobSizeIndex == maximumBlobSizeIndex-3)
+            {
+                updateStateCoroutine(MitosisState());
+            }
+            else if(currentBlobSizeIndex != maximumBlobSizeIndex-3)
+            {
+                updateStateCoroutine(EvolveState());
+            }
+        }
+    }
+
+    //To be used when instantiating an instance of this agent to alter the Start() behavior
+    public void setAgentToActive()
+    {
+        agentIsActive=true;
+    }
+
+    //This Function is used in the start function to soft start the agent in an idle state
+    void agentIdleStartUp()
+    {
         List<TileNode> tiles = gridGen.getGridNodes();
         foreach(TileNode tile in tiles)
         {
@@ -46,37 +107,27 @@ public class AiStateMachineAgent : MonoBehaviour
                 target = tile;
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //A way for user input to begin agent behaviour
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            target = getNewRandomDestination();
-            gameCoroutine =  StartCoroutine(TravelState(target));
-        }
-
-        // do checks for state change condition
-        if(atDestination())
-        {
-            updateStateCoroutine(EvolveState());
-        }
-        else if(lifeExpectancyReached())
-        {
-            updateStateCoroutine(DieState());
-        }
+        currentState = AgentState.Idle;
     }
 
     // This function clears the currently running state coroutine and takes in a new IEnumerator to start the new coroutine
-    void updateStateCoroutine(IEnumerator newState)
+    private void updateStateCoroutine(IEnumerator newState)
     {
         if(gameCoroutine!=null)
         {
             StopCoroutine(gameCoroutine);
         }
         gameCoroutine =  StartCoroutine(newState);
+    }
+
+     //This Function is used in the start function to immediatley start the agent in an chase state
+    private void activateAgentBehaviour()
+    {
+      
+        //target = getNewRandomDestination();
+        gameCoroutine =  StartCoroutine(TravelState(target = getNewRandomDestination()));
+        agentIsActive = true;
+        
     }
 
     // Reset the current tile path
@@ -89,7 +140,7 @@ public class AiStateMachineAgent : MonoBehaviour
     }
 
     // This function is used to work out which Tile this agent is currently sitting on or is closest to
-    TileNode calculateNearestTile()
+    private TileNode calculateNearestTile()
     {
         TileNode tileReferenceHolder = null;
         float tileDistance = float.MaxValue;
@@ -117,7 +168,7 @@ public class AiStateMachineAgent : MonoBehaviour
     }
 
     //This function gets a new TileNode at random to be set as the target destination, it also checks that the tile selected is traversable and can be reached before returning
-    TileNode getNewRandomDestination()
+    private TileNode getNewRandomDestination()
     {
         List<TileNode> tiles = gridGen.getGridNodes();
         bool suitableTileFound = false;
@@ -133,15 +184,8 @@ public class AiStateMachineAgent : MonoBehaviour
             ResetPath();
             int randomTileIndex = Random.Range(0,tiles.Count-1);
             currentFoundTile = tiles[randomTileIndex];
-           // try
-           // {
-                path = astar1.FindShortestPath(gridGen.grid,calculateNearestTile(), currentFoundTile);
-            //}
-            //catch (System.Exception e)
-           // {
-             //   Debug.Log(e+"; Path was null, trying again");
-            //}
-
+            path = astar1.FindShortestPath(gridGen.grid,calculateNearestTile(), currentFoundTile);
+    
             if((path != null)&&(currentFoundTile.type != TileType.Obstacle) && currentFoundTile!=currentPositionTile)
             {
                 suitableTileFound = true;
@@ -177,6 +221,7 @@ public class AiStateMachineAgent : MonoBehaviour
     // This coroutine simulates the blob evolving and then increases its size/scale
     public IEnumerator EvolveState()
 	{
+        agentBusy = true;
         currentState = AgentState.Evolve;
         float originalScaleX = transform.localScale.x;
         float originalScaleY = transform.localScale.y;
@@ -198,9 +243,27 @@ public class AiStateMachineAgent : MonoBehaviour
                 scaleAxisToggle = true;
             }
         }
-        transform.localScale = new Vector3(originalScaleX*1.25f,originalScaleY*1.25f, transform.localScale.z);
+        transform.localScale = new Vector3(originalScaleX*1.30f,originalScaleY*1.30f, transform.localScale.z);
         currentBlobSizeIndex++;
+        agentBusy = false;
 	}
+
+    public IEnumerator MitosisState()
+    {
+        agentBusy = true;
+        currentState = AgentState.Mitosis;
+        float  stateCountDown = 2f;
+        while(stateCountDown > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            stateCountDown -= 1f;
+        }
+        GameObject instantiatedObject = Instantiate(agent);
+        instantiatedObject.GetComponent<AiStateMachineAgent>().setAgentToActive();
+        currentBlobSizeIndex++;
+        transform.localScale = new Vector3(transform.localScale.x*0.9f,transform.localScale.y*0.9f, transform.localScale.z);
+        agentBusy = false;
+    }
 
     // This coroutine simulates the blob dying and then destroys/kills the blob game object
     public IEnumerator DieState()
@@ -222,11 +285,12 @@ public class AiStateMachineAgent : MonoBehaviour
     // *************************STATE CONDITION BOOLS***********************************
 
     // did agent blob reach its destination
-    bool atDestination()
+    private bool atDestination()
     {
-        if((target != null) && (Vector3.Distance(transform.position, target.gameObject.transform.position) <= 0.15f) && (currentState == AgentState.Chase))
+        if((target != null) && (Vector3.Distance(transform.position, target.gameObject.transform.position) <= 0.15f))
         {
-            target = null;  
+            destinationReachedCount++;  
+            target = null;
             return true;
         }
         else
@@ -236,10 +300,23 @@ public class AiStateMachineAgent : MonoBehaviour
     }
 
     // is it time for agent blob to die
-    bool lifeExpectancyReached()
+    private bool lifeExpectancyReached()
     {
         if(currentBlobSizeIndex >= maximumBlobSizeIndex && (currentState != AgentState.Die))
         {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool readyToTravel()
+    {
+        if((previousDestinationReachedCount < destinationReachedCount) && (currentState != AgentState.Chase) && (target == null) && (!agentBusy))
+        {
+            previousDestinationReachedCount++;
             return true;
         }
         else
